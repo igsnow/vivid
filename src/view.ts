e056a77e78887709bd5a79e3a4fdb4d954b402b9
igsnow <zzy716810918@gmail.com>
2020-04-29 17:21:20 +0800

    将获取到的tags数据存储到json文件中

--- src/view.ts
+++ src/view.ts
@@ -1,25 +1,44 @@
+import fs from 'fs'
+import path from 'path'
 import superagent from 'superagent'
 import cheerio from 'cheerio'
 
 // Tags的组成
 interface Tag {
-    title: string,
-    desc: string,
-    totalNum: string,
-    todayNum: string,
-    weekNum: string
+    title: string;
+    desc: string;
+    totalNum: string;
+    todayNum: string;
+    weekNum: string;
+}
+
+interface TagResult {
+    time: number;
+    data: Tag[];
+}
+
+interface Content {
+    [propName: number]: Tag[]
 }
 
 class View {
     private url = 'https://stackoverflow.com/tags'
 
     constructor() {
-        this.getTargetHtml()
+        this.init()
+    }
+
+    async init() {
+        const filePath = path.resolve(__dirname, '../data/tag.json')
+        const html = await this.getTargetHtml()
+        const tagInfo = this.getTagsInfo(html)
+        const fileContent = this.generateJsonContent(tagInfo)
+        fs.writeFileSync(filePath, JSON.stringify(fileContent))
     }
 
     async getTargetHtml() {
         const result = await superagent.get(this.url)
-        this.getTagsInfo(result.text)
+        return result.text
     }
 
     getTagsInfo(html: string) {
@@ -34,9 +53,21 @@ class View {
             const weekNum = $(ele).find('.fc-black-300').children('div:last-child').find('a:last-child').text()
             tagInfos.push({title, desc, totalNum, todayNum, weekNum})
         })
-        console.log(tagInfos);
+        return {
+            time: Date.now(),
+            data: tagInfos
+        }
     }
 
+    generateJsonContent(tagInfo: TagResult) {
+        const filePath = path.resolve(__dirname, '../data/tag.json')
+        let fileContent: Content = {}
+        if (fs.existsSync(filePath)) {
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        }
+        fileContent[tagInfo.time] = tagInfo.data
+        return fileContent
+    }
 
 }
 

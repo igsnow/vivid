e8dfcd54f65ab083adeb507dd376b548d91f3276
igsnow <zzy716810918@gmail.com>
2020-04-29 12:02:51 +0800

    获取overflow页面

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,2 +1,25 @@
-let str: string = 'hi igsnow'
-console.log(str)
+import superagent from 'superagent'
+
+// tags的组成
+interface Tag {
+    title: string,
+    desc: string,
+    totalQuesNum: number,
+    todayQuesNum: number,
+    weekQuesNum: number
+}
+
+class Crowller {
+    private url = 'https://stackoverflow.com/tags'
+
+    async getRawHtml() {
+        const result = await superagent.get(this.url)
+        console.log(result);
+    }
+
+    constructor() {
+        this.getRawHtml()
+    }
+}
+
+const crowller = new Crowller()
